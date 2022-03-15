9c5c46047a66538eda36dbbb09457ff11d78fb6f
j3ffr3yv <44720313+j3ffr3yv@users.noreply.github.com>
2022-03-14 19:36:51 -0700

    table

--- client/src/components/jsx/NewPage.jsx
+++ client/src/components/jsx/NewPage.jsx
@@ -3,9 +3,75 @@ import '../css/NewPage.css';
 
 function NewPage() {
     return (
-        <div>
-            <h1>New Page</h1>
-        </div>
+         <div>
+             {/* <h1 class = 'heading'>VehicleID - Vehicle Info</h1>
+             <label class = 'mileage'>
+                Mileage: ##,### 
+             </label>
+             <label class = 'vehicleInfo'>
+                Date Comment Mileage
+             </label>
+            <button class = 'addB'>
+                ADD
+            </button> */}
+            
+            <button class = 'back'>
+                Back
+            </button>
+            <h1 class = 'heading'>VehicleID - Vehicle Info</h1>
+            <table class = "table">
+                <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Comment</th>
+                    <th>Mileage</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                <td>1/15/2022</td>
+                <td>Oil Service</td>
+                <td>33,150</td>
+                </tr>
+                <tr>
+                <td>11/20/2021</td>
+                <td>Front Brakes</td>
+                <td>26,900</td>
+                </tr>
+                <tr>
+                <td>11/20/2021</td>
+                <td>Rotate Tires</td>
+                <td>26,900</td>
+                </tr>
+                <tr>
+                <td>11/20/2021</td>
+                <td>Replace Cabin Air Filter</td>
+                <td>26,900</td>
+                </tr>
+                <tr>
+                <td>11/20/2021</td>
+                <td>Oil Service</td>
+                <td>26,900</td>
+                </tr>
+                <tr>
+                <td>8/10/2021</td>
+                <td>Front Brakes</td>
+                <td>18,600</td>
+                </tr>
+                <tr>
+                <td>8/10/2021</td>
+                <td>Oil Service</td>
+                <td>18,600</td>
+                </tr>
+                </tbody>
+            </table>
+            <button class = 'addB'>
+                ADD
+            </button>
+         </div>
+         
+        
+        
     )
 }
 
