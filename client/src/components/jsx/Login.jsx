30a04476dbcfcfa240d13a3219833aa7a0dfd622
TheGreenThunder <47798163+TheGreenThunder@users.noreply.github.com>
2022-03-15 22:10:30 -0700

    Correct firebase integration

--- client/src/components/jsx/Login.jsx
+++ client/src/components/jsx/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/Login.css';
 import {Container, AppBar, Typography, Grow, Grid, TextField, Button} from '@material-ui/core';
+import { getDatabase, ref, set } from "firebase/database";
 
 function Login() {
     return (
@@ -9,10 +10,19 @@ function Login() {
                 <h1 className = "loginText">Login Page</h1>
                 <TextField className = "loginTextField" name = "Email" variant = "filled" label = "Email Address" margin = "normal" InputLabel inputlabelprops = {{ shrink: true}} required/>
                 <TextField className = "loginTextField" name = "Password" variant = "filled" label = "Password" margin = "normal" InputLabel inputlabelprops = {{ shrink: true}} required/>
-                <button> Submit </button>
+                <button onclick={this.writeUserData()}> Submit </button>
             </div>
         </div>
     )
 }
 
+function writeUserData(userId, name, email, imageUrl) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId), {
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  }
+  
 export default Login;
