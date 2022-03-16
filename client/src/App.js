8ac7c77d7620dc96714360d237e4aa8e5c8d3612
TheGreenThunder <47798163+TheGreenThunder@users.noreply.github.com>
2022-03-15 22:09:48 -0700

    Implemented correct Firebase integration

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,47 @@ import NewPage from './components/jsx/NewPage.jsx';
 
 import './components/css/NavBar.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import {getDatabase, ref, set} from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAxCd71aKYWvP88iVGhGfj8nKHGU3PD12c",
+  authDomain: "my-application-database-test.firebaseapp.com",
+  databaseURL: "https://my-application-database-test-default-rtdb.firebaseio.com",
+  projectId: "my-application-database-test",
+  storageBucket: "my-application-database-test.appspot.com",
+  messagingSenderId: "1035826648035",
+  appId: "1:1035826648035:web:338b5872a03739911546d6",
+  measurementId: "G-GPTK97CLDX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//Get a database reference 
+//https://firebase.google.com/docs/database/web/read-and-write#get_a_database_reference
+
+const database = getDatabase();
+
+//Write data https://firebase.google.com/docs/database/web/read-and-write#write_data
+
+function writeUserData(userId, name, email, imageUrl) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId), {
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  }
+
 const App = () => {
     return (
         <Router>
@@ -16,9 +57,10 @@ const App = () => {
                     <Route path="/" element={<Home />} />
                     <Route path ="/login" element = {<Login/>} />
                     <Route path ="/newpage" element = {<NewPage/>} />
+                    
                 </Routes>   
         </Router>
     );
 }
-
 export default App;
+
