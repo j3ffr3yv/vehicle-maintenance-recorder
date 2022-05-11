052fd5852546dd4f46e31a2369bc94107e233c0f
Logan Nieto <tacogoboom@gmail.com>
2022-05-10 22:43:59 -0700

    url change test

--- client/src/App.js
+++ client/src/App.js
@@ -25,8 +25,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyAxCd71aKYWvP88iVGhGfj8nKHGU3PD12c",
   authDomain: "my-application-database-test.firebaseapp.com",
   databaseURL: "https://my-application-database-test-default-rtdb.firebaseio.com",
-  projectId: "my-application-database-test",
-  storageBucket: "my-application-database-test.appspot.com",
+  projectId: "worldfoodsvehicleDB",
+  storageBucket: "worldfoodsvehicleDB.appspot.com",
   messagingSenderId: "1035826648035",
   appId: "1:1035826648035:web:338b5872a03739911546d6",
   measurementId: "G-GPTK97CLDX"
